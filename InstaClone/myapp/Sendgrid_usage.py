55da23f87ac2aa97ade802e37d20c0ef32ae67aa
Udit <uditk53@gmail.com>
2017-08-01 18:12:59 +0530

    Login page and signup page

--- InstaClone/myapp/Sendgrid_usage.py
+++ InstaClone/myapp/Sendgrid_usage.py
@@ -7,7 +7,7 @@ from sendgrid.helpers.mail import *
 
 
 def send_response(message_payload):
-    sg = sendgrid.SendGridAPIClient(apikey='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')
+    sg = sendgrid.SendGridAPIClient(apikey='SG')
     response = sg.client.mail.send.post(request_body=message_payload)
     print(response.status_code)
     print(response.body)
